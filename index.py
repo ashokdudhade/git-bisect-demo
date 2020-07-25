06c5c147f6bf3d4b0d05253dac29d9fdbcc1b062
Ashok Dudhade <ashok.dudhade@outlook.com>
2020-07-25 08:01:54 -0500

    Initial commit

---
+++ index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def _importfile(arg):
+   print 'Import initial'
+
+def _exportfile(arg):
+   print 'Export initial'
+
+
+def main(argv):
+   try:
+      opts, args = getopt.getopt(argv,"hi:e:",["ifile=","efile="])
+   except getopt.GetoptError:
+      print 'index.py -i <importfile> -e <export file>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'index.py -i <import file> -e <export file>'
+         sys.exit()
+      elif opt in ("-i", "--import"):
+         _importfile(arg)
+      elif opt in ("-e", "--export"):
+         _exportfile(arg)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
